d997b5187be20df82eb7663a06362bb5a2801ec0
Wetshivictor2018 <wetshivictor@gmail.com>
2020-03-23 15:07:44 +0100

    UPD

---
+++ exercice5.rb
@@ -0,0 +1,24 @@
+
+#on utilise #pour faire de la concaténation, c'est pour afficher le contenue de la variable
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" #pour afficher le produit de 10, 5 et 11
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #affiche le résultat de l'opération contenu dans les accolades
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #booléen, résultat est vrai ou faux
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
---
+++ exercice6.rb
@@ -0,0 +1,8 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+#impossible d'afficher le resultat car la dernière ligne, les variables données, ne sont pas déclarées
